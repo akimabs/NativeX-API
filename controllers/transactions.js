30bfd0b470cfff038544e79d1d83fdb369896fbe
Abdul Hakim <46390089+akimabs@users.noreply.github.com>
2019-09-13 21:15:09 +0700

    Update transactions.js

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -45,10 +45,10 @@ exports.patch = (req, res) => {
         req.body, {
             where: { id: req.params.id }
         }
-    ).then(data => {
+    ).then(transaction => {
        res.send({
        message: "transaction update",
-       data
+       transaction
        })
     })
   }           
